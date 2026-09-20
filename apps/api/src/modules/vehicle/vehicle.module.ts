dce4ca9693d45b36e72d411734cb64de2690df09
BYMAX SERVICES <bymaxservices@example.com>
2026-09-20 17:00:47 +0200

    Fix vehicle save

--- apps/api/src/modules/vehicle/vehicle.module.ts
+++ apps/api/src/modules/vehicle/vehicle.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { VehicleService } from './vehicle.service';
 import { VehicleController } from './vehicle.controller';
 import { AdminModule } from '../admin/admin.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [AdminModule],
+  imports: [AdminModule, AuthModule],
   controllers: [VehicleController],
   providers: [VehicleService],
 })
